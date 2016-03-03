89953171551355545a2e22bb5f4d9cfcc1a5d3f7
jimtoepel <jim.toepel@gmail.com>
2016-03-02 21:11:17 -0800

    Initial Commit

---
+++ MontageMaker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MontageMaker
+//
+//  Created by Jim Toepel on 3/2/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
