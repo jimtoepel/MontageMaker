d7f2287a4360768d1aac7361b8d03421e43487ad
jimtoepel <jim.toepel@gmail.com>
2016-03-04 18:08:52 -0800

    got me a popover!
    Love this. Let’s go nuts inside here.

--- MontageMaker/AppDelegate.swift
+++ MontageMaker/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     
+    let popover = NSPopover()
+    
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
 
 
@@ -21,29 +23,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         if let button = statusItem.button {
             button.image = NSImage(named: "StatusBarButtonImage")
-            button.action = Selector("printQuote:")
+            button.action = Selector("togglePopover:")
         }
         
+        popover.contentViewController = QuotesViewController(nibName: "QuotesViewController", bundle: nil)
+        
+        /*
         let menu = NSMenu()
         
+
         menu.addItem(NSMenuItem(title: "PrintQuote", action: Selector("printQuote:"), keyEquivalent: "P"))
         menu.addItem(NSMenuItem.separatorItem())
         menu.addItem(NSMenuItem(title: "Quit Quotes", action: Selector("terminate:"), keyEquivalent: "q"))
         
         statusItem.menu = menu
+        */
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-    
-    func printQuote(sender: AnyObject) {
-        let quoteText = "Never surrender"
-        let quoteAuthor = "Winston Churchill"
-        
-        print("\(quoteText) - \(quoteAuthor)")
-    }
 
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
 
 }
 
---
+++ MontageMaker/QuotesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuotesViewController.swift
+//  MontageMaker
+//
+//  Created by Jim Toepel on 3/4/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+import Cocoa
+
+class QuotesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
