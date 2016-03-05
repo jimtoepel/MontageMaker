c9a2bff7bc6dc94c8f1385bd8a31e516ae6063a1
jimtoepel <jim.toepel@gmail.com>
2016-03-05 11:43:39 -0800

    set up data structure of text
    Printable doesn’t work, let’s find something better.

---
+++ MontageMaker/Quote.swift
@@ -0,0 +1,36 @@
+//
+//  Quote.swift
+//  MontageMaker
+//
+//  Created by Jim Toepel on 3/5/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+import Foundation
+
+
+struct Quote {
+    let text: String
+    let author: String
+    
+    
+    static let all: [Quote] = [
+        Quote(text: "Never Put off until tomorrow what you an do the day after tomorrow.", author: "Mark Twain"),
+        Quote(text: "Efficiency is doing better what is already being done.", author: "Peter Drucker"),
+        Quote(text: "To infinity and beyond!", author: "Buzz Lightyear"),
+        Quote(text: "May the force be with you.", author: "Han Solo"),
+        Quote(text: "Simplicity is the ultimate sophistication", author: "Leonardo da Vinci"),
+        Quote(text: "It's not just what it looks like and feels like. Design is how it works.", author: "Steve Jobs")
+    ]
+ // MARK: - Printable
+}
+
+
+/*
+
+extension Quote: CustomStringConvertible {
+    var descriptions: String {
+        return "\"\(text)\" - \(author)"
+    }
+}
+ */
